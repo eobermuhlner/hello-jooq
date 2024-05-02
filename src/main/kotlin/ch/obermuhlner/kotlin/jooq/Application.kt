159005c7258ba7f1e3935a017cf380e9c6bb7432
Eric Obermühlner <eobermuhlner@gmail.com>
2024-05-02 15:34:46 +0200

    count rows to get more unique example names

--- src/main/kotlin/ch/obermuhlner/kotlin/jooq/Application.kt
+++ src/main/kotlin/ch/obermuhlner/kotlin/jooq/Application.kt
@@ -10,8 +10,11 @@ fun main() {
     val connection = DriverManager.getConnection("jdbc:h2:file:~/hello-jooq-db;DB_CLOSE_DELAY=-1", "sa", "")
     val dsl: DSLContext = DSL.using(connection)
 
-    val newExample = dsl.newRecord(Tables.EXAMPLE)
-    newExample.name = "Eric"
+    val count = dsl.selectCount().from(EXAMPLE).fetchOne(0, Int::class.java) ?: 0
+    val exampleIndex = count + 1
+
+    val newExample = dsl.newRecord(EXAMPLE)
+    newExample.name = "Eric-$exampleIndex"
     newExample.store()
 
     // Fetching data
